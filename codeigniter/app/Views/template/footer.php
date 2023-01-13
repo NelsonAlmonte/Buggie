18f98533b0b9f3e87b94adca523106536df392ca
Nelson Almonte <almontetejedanelson@gmail.com>
2023-01-13 15:01:46 -0400

    added bootstrap, added initial template

---
+++ codeigniter/app/Views/template/footer.php
@@ -0,0 +1,3 @@
+  <script src="/assets/third-party/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ codeigniter/app/Views/template/header.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Buggie</title>
+  <link href="/assets/third-party/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <link href="/assets/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- <header class="navbar navbar-expand-lg bd-navbar sticky-top py-3">
+    <nav class="container-fluid bd-gutter flex-wrap flex-lg-nowrap" aria-label="Main navigation">
+      <div class="bd-navbar-toggle">
+        <button class="navbar-toggler p-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#bdSidebar"
+          aria-controls="bdSidebar" aria-label="Toggle docs navigation">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="bi" fill="currentColor"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z">
+            </path>
+          </svg>
+          <span class="d-none fs-6 pe-1">Browse</span>
+        </button>
+      </div>
+
+      <a class="navbar-brand p-0 me-0 me-lg-2" href="/" aria-label="Bootstrap">
+        <i class="bi bi-bug me-2"></i> Buggie
+      </a>
+
+      <div class="d-flex">
+        <button class="navbar-toggler d-flex d-lg-none order-3 p-2" type="button" data-bs-toggle="offcanvas"
+          data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-label="Toggle navigation">
+          <i class="bi bi-three-dots"></i>
+        </button>
+      </div>
+
+      <div class="offcanvas-lg offcanvas-end flex-grow-1" tabindex="-1" id="bdNavbar"
+        aria-labelledby="bdNavbarOffcanvasLabel" data-bs-scroll="true">
+        <div class="offcanvas-header px-4 pb-0">
+          <h5 class="offcanvas-title text-white" id="bdNavbarOffcanvasLabel">[Project name]</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"
+            data-bs-target="#bdNavbar"></button>
+        </div>
+
+        <div class="offcanvas-body p-4 pt-0 p-lg-0 ms-sm-5">
+          <hr class="d-lg-none text-white-50">
+          <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
+            <li class="nav-item col-6 col-lg-auto">
+              <a class="nav-link py-2 px-0 px-lg-2" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item col-6 col-lg-auto">
+              <a class="nav-link py-2 px-0 px-lg-2" href="#">Issues</a>
+            </li>
+            <li class="nav-item col-6 col-lg-auto">
+              <a class="nav-link py-2 px-0 px-lg-2" href="#">Documents</a>
+            </li>
+            <li class="nav-item col-6 col-lg-auto">
+              <a class="nav-link py-2 px-0 px-lg-2" href="#">Users</a>
+            </li>
+          </ul>
+
+          <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+            <li class="nav-item col-6 col-lg-auto">
+              <a class="nav-link py-2 px-0 px-lg-2" href="#">
+                Logged user
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div class="container-fluid bd-gutter mt-3 bd-layout">
+    <aside class="bd-sidebar">
+      <div class="offcanvas-lg offcanvas-start" tabindex="-1" id="bdSidebar" aria-labelledby="bdSidebarOffcanvasLabel">
+        <div class="offcanvas-header border-bottom">
+          <h5 class="offcanvas-title" id="bdSidebarOffcanvasLabel">Buggie</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"
+            data-bs-target="#bdSidebar"></button>
+        </div>
+        <div class="offcanvas-body">
+          <nav class="bd-links w-100" id="bd-docs-nav" aria-label="Docs navigation">
+            <ul class="bd-links-nav list-unstyled mb-0 pb-3 pb-md-2 pe-lg-2">
+              <li class="bd-links-group py-2">
+                <ul class="list-unstyled fw-normal">
+                  <li><a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i
+                        class="bi bi-house me-2"></i> Home</a></li>
+                  <li><a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i
+                        class="bi bi-briefcase me-2"></i> Projects</a></li>
+                  <li><a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i
+                        class="bi bi-calendar3 me-2"></i> Calendar</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </aside>
+  </div> -->
+
+  <div id="wrapper">
+    <aside>
+      <ul class="navbar-nav sidebar py-3 px-4" id="accordionSidebar">
+        <a class="sidebar-brand d-flex text-white mb-4" href="#">
+          <i class="bi bi-bug me-2"></i> Buggie
+        </a>
+
+        <li class="nav-item">
+          <a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i class="bi bi-house me-2"></i> Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i class="bi bi-briefcase me-2"></i>
+            Projects</a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="sidebar-link d-inline-block rounded w-100 mt-1"><i class="bi bi-calendar3 me-2"></i>
+            Calendar</a>
+        </li>
+      </ul>
+    </aside>
+
+    <div id="content-wrapper" class="d-flex flex-column">
+      <main id="content">
+        <header>
+          <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+              <i class="fa fa-bars"></i>
+            </button>
+          </nav>
+        </header>
+
+        <div class="container-fluid">
+          <h1>Lorem, ipsum.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet natus voluptatum repellendus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ut labore voluptate voluptatem facilis
+            voluptatum
+            beatae, nihil tenetur nisi impedit asperiores ex quasi accusantium.</p>
+          <h2>Lorem, ipsum dolor.</h2>
+        </div>
+      </main>
+    </div>
+  </div>
+
+  <!-- <main class="bd-main order-1">
+    <div class="container-fluid">
+      <h1>Lorem, ipsum.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet natus voluptatum repellendus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ut labore voluptate voluptatem facilis
+        voluptatum
+        beatae, nihil tenetur nisi impedit asperiores ex quasi accusantium.</p>
+      <h2>Lorem, ipsum dolor.</h2>
+    </div>
+  </main>
+  </div> -->
